702193a8651bd4ad98d17cf5166be47a1c315124
jim-low <robil12380@gmail.com>
2022-03-03 14:04:46 +0800

    fix: switch to p5

--- main.js
+++ main.js
@@ -1,13 +1,8 @@
 import './style.css'
-import * as THREE from 'three'
+import p5 from 'p5'
+import { sketch } from 'p5js-wrapper'
 
-const scene = new THREE.Scene()
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-const renderer = new THREE.WebGLRenderer({
-    canvas: document.getElementById('canvas')
-})
-
-renderer.setPixelRatio(window.devicePixelRatio)
-renderer.setSize(window.innerWidth, window.innerHeight)
-camera.position.setZ(30)
-renderer.render(scene, camera)
+sketch.setup = () => {
+    createCanvas(window.innerWidth, window.innerHeight)
+    background(30)
+}
