df225fe86638fef9a70ca38e7cf9dfab0a6f9f5f
jim-low <robil12380@gmail.com>
2022-03-03 21:43:03 +0800

    feat: reduce density based on canvas width

--- main.js
+++ main.js
@@ -35,7 +35,7 @@ function increaseColor() {
 
 function spawnPoints() {
     points.length = 0
-    const density = 30
+    const density = width <= 650 ? 15 : 30
     const space = width / density
 
     for (let x = 0; x < width; x += space) {
