a3cef994650123f1c72392e1ce3b9013c901d8d3
jim-low <robil12380@gmail.com>
2022-03-03 18:22:57 +0800

    feat: add increment to color

--- main.js
+++ main.js
@@ -4,6 +4,34 @@ import { sketch } from 'p5js-wrapper'
 
 const points = []
 const mult = (Math.random() * (0.01 - 0.005) + 0.005)
+const color = {
+    r: Math.floor(Math.random() * (255 - 50) + 50),
+    g: Math.floor(Math.random() * (255 - 50) + 50),
+    b: Math.floor(Math.random() * (255 - 50) + 50),
+}
+
+
+function increaseColor() {
+    const incrementor = 0.1
+
+    color.r += incrementor
+
+    color.g += incrementor
+
+    color.b += incrementor
+
+    if (color.r < 50) {
+        color.r = 50
+    }
+
+    if (color.g < 50) {
+        color.g = 50
+    }
+
+    if (color.b < 50) {
+        color.b = 50
+    }
+}
 
 sketch.setup = () => {
     createCanvas(window.innerWidth, window.innerHeight)
@@ -27,16 +55,16 @@ sketch.draw = () => {
     fill(255)
 
     for (const point of points) {
-        const color = {
-            r: map(point.x, 0, width,  50, 255),
-            g: map(point.y, 0, height, 50, 255),
-            b: map(point.x, 0, width,  50, 255)
-        }
-        fill(color.r, color.g, color.b)
+        const r = map(point.x, 0, width,  color.r, color.r)
+        const g = map(point.y, 0, height,  color.g, color.g)
+        const b = map(point.x, 0, width,  color.b, color.b)
+        fill(r, g, b)
 
         const angle = map(noise(point.x * mult, point.y * mult), 0, 1, 0, 720)
         point.add(createVector(cos(angle), sin(angle)))
 
         ellipse(point.x, point.y, 1)
     }
+
+    increaseColor()
 }
